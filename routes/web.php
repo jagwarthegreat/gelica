d5b6b45c79c674c85f64cddf773e252caee00569
jagtheripper <jguapin21@gmail.com>
2021-03-06 20:02:14 +0800

    added families

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/families', function () {
+    return view('families');
+})->name('families');
